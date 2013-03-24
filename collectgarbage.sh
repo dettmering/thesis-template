5b3a5e244e18d9c7cd7f6e3238810eb6a006b652
Till Dettmering <prinzipiell@gmail.com>
2013-03-24 12:37:56 +0100

    .lot .lof .out files to collectgarbage.sh

--- collectgarbage.sh
+++ collectgarbage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script removes all unnecessary LaTeX files.
 
-rm *.aux *.log *.bbl *.blg *.idx *.tdo *.toc *.ilg *.ind *~
+rm *.aux *.log *.bbl *.blg *.idx *.tdo *.toc *.ilg *.ind *~ *.lof *.lot *.out
 rm *.synctex.gz
 rm *.pdf
